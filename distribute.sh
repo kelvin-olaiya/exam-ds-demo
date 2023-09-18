d3284ffe1947fe08c89d6a1453fd900aa26e4570
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-09-18 17:34:17 +0200

    chore: add demo files

---
+++ distribute.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar alchemist.jar run client/client-config.yml --verbosity debug | grep it.unibo.alchemist
---
+++ server/startup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar alchemist.jar run server-config.yml --verbosity debug | grep it.unibo.alchemist
