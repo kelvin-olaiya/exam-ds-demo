b2182706fb0152a99070b820fc4c5ac5b165797f
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-09-18 17:57:10 +0200

    chore: add run bash script

---
+++ runDemo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ ! -f alchemist.jar ]
+then
+    wget https://github.com/kelvin-olaiya/exam-ds-demo/releases/latest/download/alchemist.jar
+    if [ $? -ne 0 ]
+    then
+        exit 1
+    fi
+fi
+
+
+
+# start infrastructure
+docker compose build
+docker compose up -d
+
+# start client
+./distribute.sh
